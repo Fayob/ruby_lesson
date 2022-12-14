a9b2ab2542d2c8e365c24ee107551b2d8af3f438
fayob <adedejifavour6@gmail.com>
2022-12-14 16:10:11 +0100

    Work with puts, p and print in ruby

---
+++ Getting_started/puts_method.rb
@@ -0,0 +1,20 @@
+puts 'Hello World!!!'; 
+puts "I'm alive and well"
+puts 'Trying things out in vscode'
+puts 'New to the world of ruby'
+puts 5
+puts '4' + '3'
+
+puts
+
+puts 'See line breaks above this sentence'
+
+p 5
+p 'ertyu'
+p true
+
+p 12 / 5
+p 12.0 / 5
+p 12 / 5.0
+
+p 2**4
