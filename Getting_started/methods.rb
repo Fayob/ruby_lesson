732a4e018b3d8b3500786b2a8d19409ac10befde
fayob <adedejifavour6@gmail.com>
2022-12-14 16:07:24 +0100

    Add some methods in ruby

---
+++ Getting_started/methods.rb
@@ -0,0 +1,10 @@
+p 'hello world'.length
+
+name = 'John Doe'
+game = 'CHESS'
+
+p name.length
+p name.upcase
+p game.downcase
+
+puts name.inspect # using p to print out information to the console is using puts and .inspect under the hood
