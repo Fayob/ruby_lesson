4343a01dcbe70ada81ef0a828f70355641b8fa18
fayob <adedejifavour6@gmail.com>
2022-12-16 12:11:14 +0100

    Add arithmetic methods

---
+++ 02-Numbers/arithmetic_method.rb
@@ -0,0 +1,25 @@
+p 1 + 2
+p 1.+(2) # this is the same as p 1 + 2
+
+puts
+
+p 12 - 2
+p 12.-(2) 
+
+puts 
+
+p 23 * 4
+p 23.*(4)
+
+puts 
+
+p 10 / 5
+p 10./(5)
+p 10.0./(5.0)
+p 10.div(5)
+
+puts 
+
+p 100 % 3
+p 100.%(3)
+p 100.modulo(3)
