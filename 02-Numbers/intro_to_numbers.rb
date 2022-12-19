4e455d6be33beeeac7829b5ca0c913071f892550
fayob <adedejifavour6@gmail.com>
2022-12-19 22:28:36 +0100

    Add introduction to number file

---
+++ 02-Numbers/intro_to_numbers.rb
@@ -0,0 +1,11 @@
+#  This values belongs to a class Integer
+# There are some methods used on integer that can not be use on float numbers eg .next
+p 5.class
+p 0.class
+p 100.class
+p -34.class
+
+# This values belongs to class Float
+p 5.9343.class
+p 3.245.class
+p -9.453.class
