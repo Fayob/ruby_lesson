a072b07095104ff88f728996fe246ad429c55e11
fayob <adedejifavour6@gmail.com>
2022-12-19 22:27:52 +0100

    Add float method

---
+++ 02-Numbers/float_method.rb
@@ -0,0 +1,23 @@
+# floor method
+p 10.5.floor
+p 10.5.floor.class
+
+puts
+
+# ceil method
+p 10.5.ceil
+p 10.5.ceil.class
+
+puts 
+
+# round method
+p 3.14159.round(3)
+p 3.14159.round(3).class
+p 3.5.round
+p 3.5.round.class
+p 3.4.round
+p 3.1.round(3)
+
+# absolute method
+p 35.67.abs
+p -35.67.abs
