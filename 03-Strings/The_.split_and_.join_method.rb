6791708adc89767505137b5c4df6eca704817b9a
fayob <adedejifavour6@gmail.com>
2022-12-23 19:21:50 +0100

    Add split and join method

---
+++ 03-Strings/The_.split_and_.join_method.rb
@@ -0,0 +1,14 @@
+sentence = "Hi, my name is Boris.  There are spaces here!"
+
+p sentence.split
+p sentence.split('')
+p sentence.split(' ')
+p sentence.split('  ')
+
+# join method 
+
+names = ["Joe", "Doe", "Moe", "Bob"]
+
+p names.join
+p names.join('-')
+p names.join(',')
