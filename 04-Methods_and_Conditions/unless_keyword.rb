e49b8868381d389bfe4e3a71633c74233a658964
fayob <adedejifavour6@gmail.com>
2023-01-02 18:36:22 +0100

    Add unless conditional statement

---
+++ 04-Methods_and_Conditions/unless_keyword.rb
@@ -0,0 +1,19 @@
+password = 'password'
+
+unless password.include?('q')
+  p "Wrong Password"
+else
+  p 'Correct Password'
+end
+
+unless password.include?('a')
+  p "Wrong Password"
+else
+  p 'Correct Password'
+end
+
+unless password.include?('draw')
+  p "Wrong Password"
+else
+  p 'Correct Password'
+end
